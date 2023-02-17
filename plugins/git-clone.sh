76bd8a151628af693d845a011df64a0c2104e12b
Paul Cunningham <paulc@alliedstore.com>
2023-02-17 15:22:47 +1030

    working

--- plugins/git-clone.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-mkdir -p /opt/gitplugins/cronicle && cd /opt/gitplugins/cronicle
-git pull https://github.com/pauldcunningham/cronicle.git
-mkdir -p /opt/cronicle/bin/custom
-chmod +x /opt/gitplugins/cronicle/plugins/*
-cp /opt/gitplugins/cronicle/plugins/* /opt/cronicle/bin/custom
--- plugins/git-plugins.sh
+++ plugins/git-plugins.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+HTTP_GIT_PATH=https://github.com/pauldcunningham/cronicle.git
+
 WKDIR=`mktemp -d`
 cd $WKDIR
 mkdir repo
 git clone $HTTP_GIT_PATH repo
-mkdir -p /opt/croncile/bin/custom
+mkdir -p /opt/cronicle/bin/custom
 cp repo/plugins/* /opt/cronicle/bin/custom
-chmod +x /opt/croncile/bin/custom/*
+chmod +x /opt/cronicle/bin/custom/*
 
 echo 1
