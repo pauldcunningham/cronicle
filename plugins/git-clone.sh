54afabb3fff8bc217065684d50d902e4d4f86cc5
Paul Cunningham <paulc@alliedstore.com>
2023-02-17 13:05:52 +1030

    bootstrap

--- plugins/git-clone.sh
+++ plugins/git-clone.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 mkdir -p /opt/gitplugins && cd /opt/gitplugins
-git remote add -f origin $GIT_LOCATION
+git remote add -f origin https://github.com/pauldcunningham/cronicle.git
 git pull
 mkdir -p /opt/cronicle/bin/custom
 chmod +x cronicle/plugins/*
