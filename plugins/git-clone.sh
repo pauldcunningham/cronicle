32986f7ab46d725fa9b38bed2a2d44830c901d83
Paul Cunningham <paulc@alliedstore.com>
2023-02-17 14:39:17 +1030

    running

--- plugins/git-clone.sh
+++ plugins/git-clone.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd /opt/gitplugins/cronicle
+mkdir -p /opt/gitplugins/cronicle && cd /opt/gitplugins/cronicle
 git pull https://github.com/pauldcunningham/cronicle.git
 mkdir -p /opt/cronicle/bin/custom
 chmod +x /opt/gitplugins/cronicle/plugins/*
